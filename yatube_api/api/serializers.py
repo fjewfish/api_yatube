35d5b7eb4c80ae0459715760e831b02f063a4195
Denis Lunev <metenoeducation@yandex.ru>
2022-08-02 23:36:59 +0300

    first review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -7,14 +7,14 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+        fields = '__all__'
         read_only_fields = ('id', 'pub_date', 'author')
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -22,5 +22,5 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         read_only_fields = ('id', 'author', 'post', 'created')
